845bad23377ccf7c0e523ebc6a51241ffa152680
Brian DeConinck <bpdeconi@ncsu.edu>
2019-05-24 09:53:26 -0400

    Progress toward mutation observer

--- src/ub-php.js
+++ src/ub-php.js
@@ -3,6 +3,7 @@ import UtilityBar from './UtilityBar';
 class loadUb {
 
     constructor() {
+
         this.ubScriptSrc = this.scriptSrc;
         this.options = {};
         this.validProps = [
@@ -19,8 +20,59 @@ class loadUb {
 
         window._ub = new UtilityBar(this.options);
         window.addEventListener('DOMContentLoaded', () => {
-            window._ub.render();
+
+
+            console.log('0');
+
+        var targetNode = document.body;
+
+        if(targetNode){
+        
+            console.log('1');
+
+            var config = {
+                childList: true,
+                subtree: true
+            };
+
+            var obsCallback = function(mutationsList) {
+
+                console.log('2');
+
+                var myBarPlaceholder = document.getElementById('ncstate-utility-bar');
+
+                for(var mutation of mutationsList) {
+
+                    console.log('3');
+
+                    if ( myBarPlaceholder && !document.getElementsByClassName('ncstate-utility-bar-tools') ) {
+                        console.log( '4' );
+
+                        window._ub.render(); // THIS IS THE IMPORTANT LINE THAT RENDERS THE BRAND BAR
+                        
+                    } else{
+                        console.log('5');
+
+                    // Stop observing, we did what we needed
+                    observer.disconnect();
+                  }
+                }
+            };
+            console.log('6');
+
+
+            // Create a new observer
+            var observer = new MutationObserver(obsCallback);
+
+            // Start observing
+            observer.observe(targetNode, config);
+        
+        }
+
+        console.log('7');
+
         }, false);
+
     }
 
     get scriptSrc() {
